9403538057730baa8e9270c200be636281224c2f
Hakan Civelek <hakancivelek5461@gmail.com>
2024-04-08 00:27:52 +0300

    Browser's cross exception solved!

--- src/main/java/com/b2bshop/project/controller/LoginController.java
+++ src/main/java/com/b2bshop/project/controller/LoginController.java
@@ -7,10 +7,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Map;
 
 @RestController
