ff9d72ad9375418dde07c3f1db683f3dae01a753
Hakan Civelek <hakancivelek5461@gmail.com>
2024-04-08 18:25:01 +0300

    fix

--- src/main/java/com/b2bshop/project/controller/ShopController.java
+++ src/main/java/com/b2bshop/project/controller/ShopController.java
@@ -6,6 +6,7 @@ import com.b2bshop.project.repository.ShopRepository;
 import com.b2bshop.project.service.ShopService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
--- src/main/java/com/b2bshop/project/controller/UserController.java
+++ src/main/java/com/b2bshop/project/controller/UserController.java
@@ -6,6 +6,7 @@ import com.b2bshop.project.repository.UserRepository;
 import com.b2bshop.project.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
