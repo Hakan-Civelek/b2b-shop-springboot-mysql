a5f26ee58a97c1c88ddcb1a86102118a888c9c0e
M. Esat Akpunar <akpunar@reybex.com>
2024-04-17 00:18:49 +0300

    remove unneeded allowed controls

--- src/main/java/com/b2bshop/project/config/WebMvcConfig.java
+++ src/main/java/com/b2bshop/project/config/WebMvcConfig.java
@@ -9,9 +9,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                .allowCredentials(true);
+        registry.addMapping("/**");
     }
 }
