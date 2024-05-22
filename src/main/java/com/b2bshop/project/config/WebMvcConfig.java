453dd80c7581bf78457b625e26bba1ec704b02b8
Hakan CİVELEK <hakancivelek5461@gmail.com>
2024-05-22 20:27:01 +0300

    update for WebMvcConfig

--- src/main/java/com/b2bshop/project/config/WebMvcConfig.java
+++ src/main/java/com/b2bshop/project/config/WebMvcConfig.java
@@ -9,6 +9,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry
+                .addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
